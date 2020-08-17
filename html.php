b743886b33a530ec981da8f578a372324e383310
Vimlesh Kumar <37749343+kumarvimlesh@users.noreply.github.com>
2020-08-17 21:55:36 +0530

    working editor for html css and javascript

---
+++ html.php
@@ -0,0 +1,15 @@
+                                        <!--html code-->    	   
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <h3>Your Output Will be display here!</h3>
+        <p id="sum">hi</p>
+
+        <script src="script.js"></script>
+    </body>
+</html>
+            	                    	                    	                    	                    	                
